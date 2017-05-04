3d64276e8cd41746e61b4b4a146b24e9fd996642
Jose Luis Ballesteros <joseluis.ballesteros@tecnilogica.com>
2017-05-04 12:22:00 +0200

    Add Subject and From fields to the Webform content type

--- assets/amnistia_form_mobile.js
+++ assets/amnistia_form_mobile.js
@@ -1,17 +1,16 @@
 (function ($) {
   Drupal.behaviors.myajax = {
     attach: function(context,settings){
-      $('.webform-component--email', context).once().focusout(function(e){
+     // $('.webform-component--email', context).once().focusout(function(e){
+      $('[class*=" webform-component-"]', context).once().focusout(function(e) {
         $(this).addClass('ajaxafy');
         e.preventDefault();
-        console.log("IN");
         $.ajax({
           type: "POST",
           url: Drupal.settings.basePath + 'custom/ajax-work',
           data: $('.amnistia_form_mobile').serialize(), // you can also pass block name and act more dynamicly
           success: function (data){
-            console.log(data);
-            Drupal.attachBehaviors(document);                                      
+            Drupal.attachBehaviors(document);
           },
           error: function(data){
             console.log(data);
