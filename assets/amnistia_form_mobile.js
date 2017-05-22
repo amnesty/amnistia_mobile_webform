534d06fc4a752d70a4c290b513c080744aca4790
andres.cuervo <andres.cuervo@tecnilogica.com>
2017-05-22 17:57:02 +0200

    Json send only when a change was succes

--- assets/amnistia_form_mobile.js
+++ assets/amnistia_form_mobile.js
@@ -1,20 +1,28 @@
 (function ($) {
   Drupal.behaviors.myajax = {
     attach: function(context,settings){
-     
+ 
       $('[id*="edit-submitted"]', context).once('myajax').focusout(function(event) {
 
-        $.ajax({
-          type: "POST",
-          url: Drupal.settings.basePath + 'custom/ajax-work',
-          data: $('.amnistia_form_mobile').serialize(), // you can also pass block name and act more dynamicly
-          success: function (data){
-            Drupal.attachBehaviors(document);
-          },
-          error: function(data){
-            // console.log(data);
-          }
-        });
+        if(sessionStorage.getItem('amnistiaform_mobile') === null) {
+          sessionStorage.setItem('amnistiaform_mobile', $('.amnistia_form_mobile').serialize());
+        }
+
+        if(sessionStorage.getItem('amnistiaform_mobile') !== $('.amnistia_form_mobile').serialize()) {
+          
+          sessionStorage.setItem('amnistiaform_mobile', $('.amnistia_form_mobile').serialize());
+          $.ajax({
+            type: "POST",
+            url: Drupal.settings.basePath + 'custom/ajax-work',
+            data: $('.amnistia_form_mobile').serialize(), // you can also pass block name and act more dynamicly
+            success: function (data){
+              Drupal.attachBehaviors(document);
+            },
+            error: function(data){
+              // console.log(data);
+            }
+          });
+        }
       });
     }
   };
