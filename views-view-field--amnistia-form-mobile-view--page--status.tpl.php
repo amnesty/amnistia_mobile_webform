a3722fdb2cef48fd723fa6422f014153ac740de3
Angel Alonso <angel@ebenum.es>
2017-05-22 19:15:13 +0200

    Added a TPL to format the view_status values

---
+++ views-view-field--amnistia-form-mobile-view--page--status.tpl.php
@@ -0,0 +1,18 @@
+<?php 
+  switch($output) {
+    case '0': 
+      print t('Incomplete');
+      
+      break;
+    case '1': 
+      print t('Remember');
+      
+      break;
+    case '2':
+      print t('Complete');
+      
+      break;
+    default:
+      print $output;
+  }
+?>
